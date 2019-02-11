98bffb8948f336b63b353ccf346bf73f625215d6
wilfredinni <carlos.w.montecinos@gmail.com>
2019-02-11 11:16:54 -0300

    eslint

--- web/js/app.js
+++ web/js/app.js
@@ -1,1 +1,6 @@
-import 'mini.css';
+import "mini.css";
+import * as showdown from "showdown";
+
+const converter = new showdown.Converter();
+const text = "#hello, markdown!";
+console.log(converter.makeHtml(text));
