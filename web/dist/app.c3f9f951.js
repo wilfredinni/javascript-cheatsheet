55751754ee0553addd76ac736838900b436be3d9
wilfredinni <carlos.w.montecinos@gmail.com>
2019-02-12 16:34:49 -0300

    Minor fixes

--- web/dist/app.c3f9f951.js
+++ web/dist/app.c3f9f951.js
@@ -5501,7 +5501,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51876" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52188" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
