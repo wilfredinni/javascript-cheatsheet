3c1a506b06f37ab817d74bbaaca1e47741369bda
wilfredinni <carlos.w.montecinos@gmail.com>
2019-02-13 12:03:37 -0300

    dist

--- web/dist/app.c3f9f951.js
+++ web/dist/app.c3f9f951.js
@@ -5502,7 +5502,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50990" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51757" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
