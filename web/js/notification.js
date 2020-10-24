41e7190ce1f593b7fb69798820fb5b06c1e4b458
carlos <carlos.w.montecinos@gmail.com>
2020-10-24 18:10:32 -0300

    remove old website

--- web/js/notification.js
+++
@@ -1,9 +0,0 @@
-// index message
-let message = document.getElementById("HomeMessage");
-let closeNotificationBtn = document.getElementById("closeNotification");
-
-// functions
-const closeMessage = () => (message.style.display = "none");
-
-// listeners
-closeNotificationBtn.addEventListener("click", closeMessage);
