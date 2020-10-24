7b036ed92b217672e8ed2e17fd59d9a16029c0d3
carlos <carlos.w.montecinos@gmail.com>
2020-10-24 18:01:24 -0300

    settings

--- website/nuxt.config.js
+++ website/nuxt.config.js
@@ -3,6 +3,9 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
+  router: {
+    base: '/javascript-cheatsheet/website',
+  },
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
@@ -77,4 +80,5 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
+  ssr: false,
 }
