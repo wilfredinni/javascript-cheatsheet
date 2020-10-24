898e532ca426ac81dca1643d969fcf3c7f8520e2
carlos <carlos.w.montecinos@gmail.com>
2020-10-24 18:50:01 -0300

    config

--- website/nuxt.config.js
+++ website/nuxt.config.js
@@ -4,7 +4,7 @@ export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
   router: {
-    base: '/',
+    base: '/javascript-cheatsheet/website/',
   },
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
