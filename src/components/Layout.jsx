d36279e8a9430d775d37b82189f01df18eb0cec5
Carlos <carlos@pythoncheatsheet.org>
2023-08-08 20:30:34 -0400

    feat: hide hero

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -195,7 +195,7 @@ export function Layout({
     <>
       <Header navigation={navigation} />
 
-      {isHomePage && <Hero />}
+      {/* {isHomePage && <Hero />} */}
 
       <div className="relative mx-auto flex w-full max-w-8xl flex-auto justify-center sm:px-2 lg:px-8 xl:px-12">
         <div className="hidden lg:relative lg:block lg:flex-none">
