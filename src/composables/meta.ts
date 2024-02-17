5c659f6240204eaf5eb599162eb9f71afa86fd8e
Carlos <carlos@pythoncheatsheet.org>
2024-02-17 14:38:05 -0300

    Commented out cardImage URLs in useMeta function

--- src/composables/meta.ts
+++ src/composables/meta.ts
@@ -4,8 +4,8 @@ export function useMeta() {
 
   const description =
     'Anyone can forget how to make character classes for a regex, slice a list or do a for loop. This cheat sheet tries to provide a basic reference for beginner and advanced developers, lower the entry barrier for newcomers and help veterans refresh the old tricks.'
-  const cardImage =
-    'https://raw.githubusercontent.com/wilfredinni/javascript-cheatsheet/master/public/social/social_banner.png'
+  // const cardImage =
+  // 'https://raw.githubusercontent.com/wilfredinni/javascript-cheatsheet/master/public/social/social_banner.png'
   const themeColor = computed(() => (isDark.value ? '#1f2937' : '#ffffff'))
   const url = computed(() => `https://${base_url}${route.path}`)
   const keywords = [
@@ -49,10 +49,10 @@ export function useMeta() {
       { property: 'og:description', content: description },
       { property: 'og:url', content: url },
       { property: 'og:type', content: 'article' },
-      { property: 'og:image', content: cardImage },
+      // { property: 'og:image', content: cardImage },
       { name: 'twitter:title', content: 'Javascript Cheatsheet' },
       { name: 'twitter:description', content: 'The Javascript Cheatsheet' },
-      { name: 'twitter:image', content: cardImage },
+      // { name: 'twitter:image', content: cardImage },
       { name: 'twitter:card', content: 'summary' },
     ],
     link: [{ rel: 'canonical', href: url }],
