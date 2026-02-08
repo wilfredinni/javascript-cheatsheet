b1b03078fc73f952fd20adee27aaf5e4e1710a5e
Carlos <carlos@pythoncheatsheet.org>
2026-02-08 19:42:27 -0300

    Remove PWA support and related configurations from the project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import path from 'path'
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig(async ({ mode }) => {
@@ -28,33 +27,6 @@ export default defineConfig(async ({ mode }) => {
     plugins: [
       react(),
       tailwindcss(),
-      VitePWA({
-        registerType: 'autoUpdate',
-        includeAssets: ['favicon.svg', 'safari-pinned-tab.svg'],
-        manifest: {
-          name: 'JavaScript Cheatsheet',
-          short_name: 'JavaScript Cheatsheet',
-          theme_color: '#ffffff',
-          icons: [
-            {
-              src: '/android-chrome-192x192.png',
-              sizes: '192x192',
-              type: 'image/png',
-            },
-            {
-              src: '/android-chrome-512x512.png',
-              sizes: '512x512',
-              type: 'image/png',
-            },
-            {
-              src: '/android-chrome-512x512.png',
-              sizes: '512x512',
-              type: 'image/png',
-              purpose: 'any maskable',
-            },
-          ],
-        },
-      }),
     ],
 
     // https://github.com/vitest-dev/vitest
