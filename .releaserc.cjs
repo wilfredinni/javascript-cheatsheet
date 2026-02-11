6361b882f8aa86b9b4f5d8617b8ce07c017a6914
Carlos <carlos@pythoncheatsheet.org>
2026-02-11 20:10:31 -0300

    feat: Set up GitHub Actions + semantic-release

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -1,24 +1,10 @@
 module.exports = {
-  branches: ['main'],
+  branches: ['master'],
   plugins: [
     ['@semantic-release/commit-analyzer', { preset: 'conventionalcommits' }],
     [
       '@semantic-release/release-notes-generator',
       { preset: 'conventionalcommits' },
-    ],
-    [
-      '@semantic-release/exec',
-      {
-        prepareCmd: 'bun scripts/update-changelog.ts',
-      },
-    ],
-    [
-      '@semantic-release/git',
-      {
-        assets: ['src/pages/changelog.md'],
-        message:
-          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-      },
     ],
     '@semantic-release/github',
   ],
